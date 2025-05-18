a1302f745013c347b4b5f851eb1018f3764240a1
Vladyslav Varlan <varlanv96@gmail.com>
2025-05-18 23:11:57 +0300

    Update plugin publishing

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -4,10 +4,16 @@ plugins {
 }
 
 gradlePlugin {
+    website = "https://github.com/varlanv/java-test-konvence"
+    vcsUrl = "https://github.com/varlanv/java-test-konvence"
     plugins {
         create("testKonvenceGradlePlugin") {
             id = "com.varlanv.test-konvence"
             implementationClass = "com.varlanv.testkonvence.gradle.plugin.TestKonvencePlugin"
+            displayName = "Test Konvence Plugin"
+            description = "Plugin that provides a way to automatically change and enforce test naming"
+            tags = listOf("test", "convention", "junit", "naming", "quality")
+
         }
     }
 }
